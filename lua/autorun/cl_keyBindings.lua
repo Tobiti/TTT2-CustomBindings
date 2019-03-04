c19a02c4bb71ecfd0cd91717140784b9de99ee89
Tobias Adloff <tadloff@mail.upb.de>
2019-03-04 15:27:04 +0100

    Add Version 0.9

---
+++ lua/autorun/cl_keyBindings.lua
@@ -0,0 +1,339 @@
+if SERVER then
+    AddCSLuaFile()
+end
+
+conCommandsToBind={}
+
+--List with all conCommands that should be binded to a key
+local bindList = {}	
+
+--FUNCTIONS to manipulate the BINDLIST
+function conCommandsToBind.Get()
+	return bindList
+end
+
+function conCommandsToBind.Add(key, concommand, name, category, activated)
+	
+	--Check if conCommands is already in the bindList
+	if bindList[key] != nil then
+		--print("Eintrag " .. key .. " bereits Vorhanden")
+		return false
+	end
+	
+	--Add the new entry
+	local newEntry = {}
+	newEntry.concommand=concommand
+	newEntry.name=name
+	newEntry.category=category
+	newEntry.activated=activated
+	
+	bindList[key]= newEntry;
+	
+	print("[TTT2][Bindings] Add new Binding Entry: " .. key .. " " .. concommand .. " " .. name .. " " .. category .. " " .. tostring(activated))
+	
+	return true
+end
+
+function conCommandsToBind.AddToServer(key, concommand, name, category, activated)
+
+	if LocalPlayer():IsSuperAdmin() then
+		if conCommandsToBind.Add(key, concommand, name, category, activated) then
+			net.Start("ttt2_cb_addNewEntry")
+			net.WriteString(key)
+			net.WriteString(concommand)
+			net.WriteString(name)
+			net.WriteString(category)
+			net.WriteBool(activated)
+			net.SendToServer()
+		end
+	end
+	
+	return false
+end
+function conCommandsToBind.AddToPlayer(key, concommand, name, category, activated, ply)
+	
+	if SERVER then
+		net.Start("ttt2_cb_addNewEntry")
+		net.WriteString(key)
+		net.WriteString(concommand)
+		net.WriteString(name)
+		net.WriteString(category)
+		net.WriteBool(activated)
+		net.Send(ply)
+	end
+	
+	return false
+end
+
+function conCommandsToBind.Remove(key)
+
+	if bindList[key] != nil then
+		bindList[key] = nil
+		return true
+	end
+	return false
+end
+
+function conCommandsToBind.RemoveFromServer(key)
+
+	if LocalPlayer():IsSuperAdmin() then
+		if conCommandsToBind.Remove(key) then
+			net.Start("ttt2_cb_removeEntry")
+			net.WriteString(key)
+			net.SendToServer()
+		end
+	end
+	
+	return false
+end
+function conCommandsToBind.RemoveFromPlayer(key, ply)
+
+	if SERVER then
+		net.Start("ttt2_cb_removeEntry")
+		net.WriteString(key)
+		net.Send(ply)
+	end
+	
+	return false
+end
+
+function conCommandsToBind.Edit(oldKey, newKey, concommand, name, category, activated)
+	if oldKey == newKey then
+		--Edit the  entry
+		bindList[newKey].concommand = concommand;
+		bindList[newKey].name = name;
+		bindList[newKey].category = category;
+		bindList[newKey].activated = activated;
+		
+		print("[TTT2][Bindings] Edit Binding Entry: " .. newKey .. " " .. concommand .. " " .. name .. " " .. category .. " " .. tostring(activated))
+		return true
+	else 
+		conCommandsToBind.Remove(oldKey)
+		return conCommandsToBind.Add(newKey, concommand, name, category, activated)
+	end
+	
+	return false
+end
+
+function conCommandsToBind.EditToServer(oldKey, newKey, concommand, name, category, activated)
+
+	if LocalPlayer():IsSuperAdmin() then
+		if conCommandsToBind.Edit(oldKey, newKey, concommand, name, category, activated) then
+			net.Start("ttt2_cb_editEntry")
+			net.WriteString(oldKey)
+			net.WriteString(newKey)
+			net.WriteString(concommand)
+			net.WriteString(name)
+			net.WriteString(category)
+			net.WriteBool(activated)
+			net.SendToServer()
+		end
+	end
+	
+	return false
+end
+
+function conCommandsToBind.EditToPlayer(oldKey, newKey, concommand, name, category, activated, ply)
+
+	if SERVER then
+		net.Start("ttt2_cb_editEntry")
+		net.WriteString(oldKey)
+		net.WriteString(newKey)
+		net.WriteString(concommand)
+		net.WriteString(name)
+		net.WriteString(category)
+		net.WriteBool(activated)
+		net.Send(ply)
+	end
+	
+	return false
+end
+
+
+if SERVER then
+	util.AddNetworkString( "ttt2_cb_addNewEntry" )
+	util.AddNetworkString( "ttt2_cb_editEntry" )
+	util.AddNetworkString( "ttt2_cb_removeEntry" )
+
+
+	function conCommandsToBind.SendListToPlayer( ply )	
+		print("[TTT2][Bindings] Send Custom Bindings to " .. ply:Name())
+		
+		for label, entry in pairs(conCommandsToBind.Get()) do
+			net.Start("ttt2_cb_addNewEntry")
+			net.WriteString(label)
+			net.WriteString(entry.concommand)
+			net.WriteString(entry.name)
+			net.WriteString(entry.category)
+			net.WriteBool(entry.activated)
+			net.Send(ply)
+		end
+	end
+		
+	hook.Add("PlayerAuthed", "TTT2BindingsCustomSync", function(ply, steamid, uniqueid)
+		conCommandsToBind.SendListToPlayer( ply )
+	end)
+	
+	local function CreateTable()
+		print("[TTT2][Bindings] Create new Table")
+		sql.Query( "CREATE TABLE ttt2_custombindings( Key TEXT NOT NULL, Name TEXT, Category TEXT, Command TEXT, Activated INTEGER, PRIMARY KEY(`Key`) )" )
+	end
+	
+	function conCommandsToBind.AddCustomBindingToSQL(key, concommand, name, category, activated)
+		local tmp = 0
+		if activated then
+			tmp = 1
+		end
+		print(sql.Query("INSERT INTO ttt2_custombindings( Key, Name, Category, Command, Activated ) VALUES( '"..key.."', '"..name.."', '"..category.."', '"..concommand.."', ".. tostring(tmp) .." )"))
+	end
+	
+	function conCommandsToBind.UpdateCustomBindingInSQL(key, concommand, name, category, activated)
+		local tmp = 0
+		if activated then
+			tmp = 1
+		end
+		
+		local result = sql.Query("UPDATE ttt2_custombindings SET Name='"..name.."', Category='"..category.."', Command='"..concommand.."', Activated='".. tostring(tmp) .."' WHERE Key='"..key.."'")
+		if result == false then
+			print("[TTT2][Bindings] Error: " .. sql.LastError( result ))
+		end
+	end
+	
+	function conCommandsToBind.RemoveCustomBindingToSQL(key)
+		local result = sql.Query("DELETE FROM ttt2_custombindings WHERE Key='"..key.."'")
+		if result == false then
+			print("[TTT2][Bindings] Error: " .. sql.LastError( result ))
+		end
+	end
+	
+	function conCommandsToBind.LoadCustomBindingFromSQL()
+		local tmp = sql.Query("SELECT * FROM ttt2_custombindings")
+		
+		for k, v in pairs(tmp) do
+			conCommandsToBind.Add(v.Key, v.Command, v.Name, v.Category, v.Activated == "1")
+		end
+	end
+	
+	if not  sql.TableExists("ttt2_custombindings") then
+		CreateTable()
+	end
+	conCommandsToBind.LoadCustomBindingFromSQL()
+end
+
+
+local function UpdateBindings()
+	if CLIENT then
+		for key, entry in pairs(bindList) do
+			if entry.activated then
+				if entry.concommand != "" then
+					bind.Register(key, function()
+						LocalPlayer():ConCommand(entry.concommand)
+					end,
+					function()
+					end)
+				end
+				bind.AddSettingsBinding(key, entry.name, entry.category)
+			end
+		end
+	end
+end
+local function RemoveBindings( key )
+	if CLIENT then
+		-- TODO: Remove Binding
+	end
+end
+
+
+net.Receive ("ttt2_cb_addNewEntry", function( length, ply )	
+
+	if SERVER and not ply:IsSuperAdmin() then
+		return
+	end
+	
+	local key = net.ReadString()
+	local concommand = net.ReadString()
+	local name = net.ReadString()
+	local category = net.ReadString()
+	local activated = net.ReadBool()
+	
+	if not conCommandsToBind.Add(key, concommand, name, category, activated) then
+		conCommandsToBind.Edit(key, key, concommand, name, category, activated)
+		if SERVER then
+			UpdateCustomBindingInSQL(key, concommand, name, category, activated)		
+			for k, p in pairs(player.GetAll()) do
+				conCommandsToBind.EditToPlayer(key, key, concommand, name, category, activated, p)
+			end
+		end
+	else
+		if SERVER then
+			conCommandsToBind.AddCustomBindingToSQL(key, concommand, name, category, activated)		
+			for k, p in pairs(player.GetAll()) do
+				conCommandsToBind.AddToPlayer(key, concommand, name, category, activated, p)
+			end
+		end
+	end
+	
+	if CLIENT then
+		UpdateBindings()
+	end
+end)
+
+net.Receive ("ttt2_cb_editEntry", function( length, ply )	
+
+	if SERVER and not ply:IsSuperAdmin() then
+		return
+	end
+	
+	local oldKey = net.ReadString()
+	local newKey = net.ReadString()
+	local concommand = net.ReadString()
+	local name = net.ReadString()
+	local category = net.ReadString()
+	local activated = net.ReadBool()
+	
+	conCommandsToBind.Edit(oldKey, newKey, concommand, name, category, activated)
+	
+	if CLIENT then
+		UpdateBindings()
+	else
+		conCommandsToBind.UpdateCustomBindingInSQL(oldKey, concommand, name, category, activated)	
+		for k, p in pairs(player.GetAll()) do
+			conCommandsToBind.EditToPlayer(oldKey, newKey, concommand, name, category, activated, p)
+		end
+	end
+end)
+
+net.Receive ("ttt2_cb_removeEntry", function( length, ply )	
+
+	if SERVER and not ply:IsSuperAdmin() then
+		return
+	end
+	
+	local key = net.ReadString()
+	
+	if conCommandsToBind.Remove(key) then
+		print("[TTT2][Bindings] Remove Entry " .. key)
+	end
+	
+	if CLIENT then
+		RemoveBindings(key)
+	else
+		conCommandsToBind.RemoveCustomBindingToSQL(key)
+		for k, p in pairs(player.GetAll()) do
+			conCommandsToBind.RemoveFromPlayer(key, p)
+		end
+	end
+end)
+
+if CLIENT then
+	hook.Add("TTT2ScoreboardAddPlayerRow", "TTTAddMelonAddonDevTobiti", function(ply)
+		local tsid64 = ply:SteamID64()
+
+		if tostring(tsid64) == "76561197989909602" then
+			AddTTT2AddonDev(tsid64)
+		end
+		if tostring(tsid64) == "76561198047056948" then
+			AddTTT2AddonDev(tsid64)
+		end
+	end)
+end
---
+++ lua/ulx/xgui/bindings.lua
@@ -0,0 +1,108 @@
+local bindings = xlib.makepanel{ parent=xgui.null }
+
+xlib.makelabel{x = 5, y = 270, w = 600, wordwrap = true, label = "Help:\n If Command is empty, the function behind the binding isn't set or overwritten!", parent = bindings}
+
+
+bindings.list = xlib.makelistview{ x=5, y=20, w=550, h=190, parent=bindings }
+bindings.list:AddColumn( "Name" )
+bindings.list:AddColumn( "Label" )
+bindings.list:AddColumn( "Category" )
+bindings.list:AddColumn( "Command" )
+bindings.list:AddColumn( "Active" )
+bindings.list:AddColumn( "Custom" )
+bindings.list.OnRowSelected = function( self, LineID, Line )
+	bindings.editlist:SetDisabled( false )
+	bindings.removelist:SetDisabled( !Line:GetColumnText(6) )
+end
+
+local function UpdateList()
+	bindings.list:Clear()
+	for id, v in pairs( conCommandsToBind.Get() ) do
+		bindings.list:AddLine( id, v.name, v.category, v.concommand, v.activated ,true )
+	end
+	for id, v in ipairs( bind.GetSettingsBindings() ) do
+		if conCommandsToBind.Get()[v.name] == nil then
+			bindings.list:AddLine( v.name, v.label, v.category, "", true, false )
+		end
+	end
+end
+UpdateList()
+
+-- Add Button
+bindings.addlist = xlib.makebutton{ x=5, y=210, w=150, label="Add New Binding", parent=bindings }
+bindings.addlist.DoClick = function()
+	bindings.addNewEntry()
+end
+
+-- Edit Button
+bindings.editlist = xlib.makebutton{ x=160, y=210, w=150, label="Edit Binding", parent=bindings, disabled=true }
+bindings.editlist.DoClick = function()
+	if bindings.list:GetSelectedLine() then
+		local key = bindings.list:GetSelected()[1]:GetColumnText(1)		
+		bindings.editEntry( key, bindings.list:GetSelected()[1]:GetColumnText(4), bindings.list:GetSelected()[1]:GetColumnText(2), bindings.list:GetSelected()[1]:GetColumnText(3), bindings.list:GetSelected()[1]:GetColumnText(5), bindings.list:GetSelected()[1]:GetColumnText(6) )
+	end
+end
+-- Remove Button
+bindings.removelist = xlib.makebutton{ x=315, y=210, w=150, label="Remove Binding", parent=bindings, disabled=true }
+bindings.removelist.DoClick = function()
+	if bindings.list:GetSelectedLine() then
+		local ID = bindings.list:GetSelected()[1]:GetColumnText(1)
+		conCommandsToBind.RemoveFromServer(ID)
+		UpdateList()
+	end
+end
+-- Refresh Button
+bindings.refreshlist = xlib.makebutton{ x=5, y=240, w=150, label="Refresh List", parent=bindings }
+bindings.refreshlist.DoClick = function()
+	UpdateList()
+end
+
+function bindings.addNewEntry()
+	local frame = xlib.makeframe{ label="Add new Binding Entry", w=300, h=180, skin=xgui.settings.skin }
+	
+	xlib.makelabel{ x=5, y=30, label="Key:", parent=frame }
+	frame.bindingKey =	xlib.maketextbox{ x=100, y=30, w=180, parent=frame, selectall=true, text="Enter key" };
+	xlib.makelabel{ x=5, y=55, label="Label:", parent=frame }
+	frame.bindingLabel =	xlib.maketextbox{ x=100, y=55, w=180, parent=frame, selectall=true, text="Enter label" };
+	xlib.makelabel{ x=5, y=80, label="Category:", parent=frame }
+	frame.bindingCategory =	xlib.maketextbox{ x=100, y=80, w=180, parent=frame, selectall=true, text="Enter category" };
+	xlib.makelabel{ x=5, y=105, label="Command:", parent=frame }
+	frame.bindingCommand =	xlib.maketextbox{ x=100, y=105, w=180, parent=frame, selectall=true, text="" };
+	
+	frame.bindingActive = xlib.makecheckbox{ x=100, y=130, label="Active", value=true, parent=frame }
+	
+	frame.apply = xlib.makebutton{ x=130, y=155, w=150, label="Add", parent=frame }
+	frame.apply.DoClick = function()
+		conCommandsToBind.AddToServer(frame.bindingKey:GetValue(), frame.bindingCommand:GetValue(), frame.bindingLabel:GetValue(), frame.bindingCategory:GetValue(), frame.bindingActive:GetChecked())
+		UpdateList()
+		frame:Remove()
+	end
+end
+
+function bindings.editEntry( key, concommand, name, category, activated, custom )
+	local frame = xlib.makeframe{ label="Edit Entry " .. name, w=300, h=180, skin=xgui.settings.skin }
+	
+	xlib.makelabel{ x=5, y=30, label="Key:", parent=frame }
+	frame.bindingKey =	xlib.maketextbox{ x=100, y=30, w=180, parent=frame, selectall=true, text=key };
+	xlib.makelabel{ x=5, y=55, label="Label:", parent=frame }
+	frame.bindingLabel =	xlib.maketextbox{ x=100, y=55, w=180, parent=frame, selectall=true, text=name };
+	xlib.makelabel{ x=5, y=80, label="Category:", parent=frame }
+	frame.bindingCategory =	xlib.maketextbox{ x=100, y=80, w=180, parent=frame, selectall=true, text=category };
+	xlib.makelabel{ x=5, y=105, label="Command:", parent=frame }
+	frame.bindingCommand =	xlib.maketextbox{ x=100, y=105, w=180, parent=frame, selectall=true, text=concommand };
+	
+	frame.bindingActive = xlib.makecheckbox{ x=100, y=130, label="Active", value=activated, parent=frame }
+	
+	frame.apply = xlib.makebutton{ x=130, y=155, w=150, label="Apply", parent=frame }
+	frame.apply.DoClick = function()
+		if custom then
+			conCommandsToBind.EditToServer(key, frame.bindingKey:GetValue(), frame.bindingCommand:GetValue(), frame.bindingLabel:GetValue(), frame.bindingCategory:GetValue(), frame.bindingActive:GetChecked())
+		else
+			conCommandsToBind.AddToServer(frame.bindingKey:GetValue(), frame.bindingCommand:GetValue(), frame.bindingLabel:GetValue(), frame.bindingCategory:GetValue(), frame.bindingActive:GetChecked())
+		end
+		UpdateList()
+		frame:Remove()
+	end
+end
+
+xgui.addModule( "Custom Bindings", bindings, "icon16/ttt.png", "xgui_gmsettings" )
